16b52b40ad7facab1429ca8d0e5eb0bd213bc71c
taylor <376525082@qq.com>
2021-09-25 13:03:17 +0800

    增加接口文档

---
+++ nora-doc/src/main/java/org/taylor/nora/doc/NoraDocApplication.java
@@ -0,0 +1,13 @@
+package org.taylor.nora.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoraDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoraDocApplication.class, args);
+    }
+
+}
---
+++ nora-job/src/main/java/org/taylor/nora/job/config/SwaggerConfiguration.java
@@ -0,0 +1,85 @@
+
+package org.taylor.nora.job.config;
+
+import com.github.xiaoymin.knife4j.core.util.CollectionUtils;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.List;
+
+
+@Configuration
+@EnableSwagger2WebMvc
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfiguration {
+
+    @Bean(value = "jobApi")
+    @Order(value = 2)
+    public Docket groupRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(groupApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.taylor.nora.job.controller"))
+                .paths(PathSelectors.any())
+
+                .build().securityContexts(CollectionUtils.newArrayList(securityContext(),securityContext1())).securitySchemes(CollectionUtils.<SecurityScheme>newArrayList(apiKey(),apiKey1()));
+    }
+
+    private ApiInfo groupApiInfo(){
+        return new ApiInfoBuilder()
+                .title("Nora job文档")
+                .description("<div style='font-size:14px;color:red;'>Nora job RESTful APIs</div>")
+                .termsOfServiceUrl("http://www.group.com/")
+                .contact("group@qq.com")
+                .version("1.0")
+                .build();
+    }
+
+
+
+    private ApiKey apiKey() {
+        return new ApiKey("BearerToken", "Authorization", "header");
+    }
+    private ApiKey apiKey1() {
+        return new ApiKey("BearerToken1", "Authorization-x", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/.*"))
+                .build();
+    }
+    private SecurityContext securityContext1() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth1())
+                .forPaths(PathSelectors.regex("/.*"))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return CollectionUtils.newArrayList(new SecurityReference("BearerToken", authorizationScopes));
+    }
+    List<SecurityReference> defaultAuth1() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return CollectionUtils.newArrayList(new SecurityReference("BearerToken1", authorizationScopes));
+    }
+
+}
